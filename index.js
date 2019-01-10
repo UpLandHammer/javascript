31b70dac4957086236f41b195bdfd41cbded28ef
Marreta <bruno.rodrigues>
2019-01-09 22:31:53 -0200

    adicionar novos itens

--- index.js
+++ index.js
@@ -13,22 +13,42 @@ botaoModo.addEventListener("click", function(){
 })
 
 function adicionaItemLista(descricao, tipoLista, idLista){
-    if(descricao.length > 0 && !existeItem(descricao)){
-            var novoItem = document.createElement(tipoLista);
-            var textoItem = document.createTextNode(descricao);
-            novoItem.appendChild(textoItem);
-            novoItem.className = "list-group-item";
-            var nodePaiLista = document.getElementById(idLista);
-            nodePaiLista.appendChild(novoItem);
+    if(descricao.length > 0 ) {
+        if(!existeItem(descricao, idLista, "list-group-item")){
+            removeItemLista("danger")
+            createItemLista(tipoLista, descricao, "list-group-item", idLista);
+        } else {
+            createItemLista("div", "O item já existe!", "alert alert-danger", idLista, "danger");
+        }
     }
-    event.preventDefault();
+
 }
 
-function existeItem(descricao){
-    var listagemItens = document.getElementsByClassName("list-group-item");
-    for(var i = 0; i < listagemItens.length; i++){
-        if(descricao === listagemItens[i].textContent){
+function existeItem(descricao, idLista, classe){
+    let parent = document.getElementById(idLista);
+    let listagemItens = parent.getElementsByClassName(classe);
+    for(let i = 0; i < listagemItens.length; i++){
+        if(descricao == listagemItens[i].textContent){
             return true;
-        }
+        } 
+    }
+}
+
+function createItemLista(elemento, descricao, classe, idLista, idItem){
+    if(!existeItem(descricao, idLista, classe)){
+        let novoItem = document.createElement(elemento);
+        let textoItem = document.createTextNode(descricao);
+        novoItem.appendChild(textoItem);
+        novoItem.className = classe;
+        novoItem.setAttribute("id", idItem);
+        let nodePaiLista = document.getElementById(idLista);
+        nodePaiLista.appendChild(novoItem);
+    }
+}
+
+function removeItemLista(id){
+    let elemento = document.getElementById(id);
+    if(elemento !== null){
+        elemento.parentNode.removeChild(elemento);
     }
 }
